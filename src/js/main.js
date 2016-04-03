e8de34093325cdcf0461e4e083cba507ec655cdf
Brian Kintz <elmugrat@gmail.com>
2016-04-03 18:05:14 +0200

    Add fade in animations for job content on scroll down

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,26 @@
-/* global $ */
+window.onbeforeunload = function() { window.scrollTo(0,0); }
 
+window.onload = function() {
+  var waypoint1 = new Waypoint({
+    element: document.getElementById('row-work-1'),
+    handler: waypointIn,
+    offset: '90%'
+  });
+
+  var waypoint1 = new Waypoint({
+    element: document.getElementById('row-work-2'),
+    handler: waypointIn,
+    offset: '90%'
+  });
+
+  var waypoint1 = new Waypoint({
+    element: document.getElementById('row-work-3'),
+    handler: waypointIn,
+    offset: '90%'
+  });
+}
+
+function waypointIn() {
+  this.element.className += " in"
+  this.destroy();
+}
