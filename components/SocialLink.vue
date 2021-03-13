492fdf19f9c21a459eda56170a6069d166feeebc
Brian Kintz <btkintz@gmail.com>
2021-03-13 22:55:56 +0100

    Got a little carried away and rewrote the site

---
+++ components/SocialLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    :href="href"
+    class="inline-block rounded focus:outline-none focus:ring ring-blue-light"
+  >
+    <FontAwesomeIcon
+      :icon="[faFamily, faIcon]"
+      class="mx-5 lg:mx-2 p-1 fa-2x text-gray-50 hover:text-blue-light transition-colors"
+      rel="noopener"
+      target="_blank"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    faFamily: {
+      type: String,
+      default: 'fab',
+    },
+    faIcon: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
